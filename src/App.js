eb728f21cd9406271a1f13d78eb5b4d21af5d15b
Hamzah-syed <50793209+Hamzah-syed@users.noreply.github.com>
2020-06-21 18:08:05 +0500

    updated layout file names

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./css/App.css";
 import "./css/tailwind.css";
 
 //layout
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import Header from "./components/layout/header";
+import Footer from "./components/layout/footer";
 
 //PAGES
 import Home from "./pages/Home";
